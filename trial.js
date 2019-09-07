4f4a119b94f5ed424028e2e2e2a3a3d58b426da3
achalaspandit <achpices@gmail.com>
2019-09-07 17:38:02 +0530

    lala

--- trial.js
+++ trial.js
@@ -10,41 +10,37 @@ for (let i = 0; i < rows; ++i)
     let tokens = lines[i].split(' ');
     result.push(tokens);
 }
-function matrix( rows, cols){
-
-    var arr = [];
-  
-    // Creates all lines:
-    for(var i=0; i < rows; i++){
-  
-        // Creates an empty line
-        arr.push([]);
-  
-        // Adds cols to the empty line:
-        arr[i].push( new Array(cols));
-    
-        
-    }
-  
-  return arr;
-  }
 for (i=0;i<rows;i++)
         {   for(j=0; j<rows; j++)
                 result[i][j]= parseInt(result[i][j]);
         }
+
+function matrix( rows, cols)
+{
+    var arr = [];
+    for(var i=0; i < rows; i++)
+    {
+        arr.push([]);
+        arr[i].push( new Array(cols));
+    }
+  
+  return arr;
+  }
+
+
 var p,u=0;
 var nearv=[50];
 var t=matrix(rows,3);
 var mincost=0;
-for(i=1;i<rows;i++)
+for(i=2;i<rows;i++)
 {
     nearv[i]=1;
 }
-nearv[0]=0;
-for(i=0;i<rows;i++)
+nearv[1]=0;
+for(i=1;i<rows;i++)
 {
     let min=999;
-    for(j=0;j<rows;j++)
+    for(j=1;j<rows;j++)
     {    p= nearv[j];
         if(nearv[j]!=0 && result[j][p]<min)
         {
@@ -53,8 +49,8 @@ for(i=0;i<rows;i++)
         }
         
     }
-    t[i][1]=++u;
-    t[i][2]=++nearv[u];
+    t[i][1]=u;
+    t[i][2]=nearv[u];
     t[i][3]=min;
     mincost+=min;
     nearv[u]=0;
@@ -66,4 +62,7 @@ for(i=0;i<rows;i++)
         }
     }
 }
+t.splice(rows - 1, 1);
+for(i=0;i<rows-1;i++)
+    t[i].splice(0,1);
 console.log(t);
